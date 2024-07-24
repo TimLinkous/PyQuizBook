71b06aac763e1495d47a49d0f3f38d5e62e593ab
Tim L <timl@Zipcoders-MacBook-Pro.local>
2024-07-24 19:38:59 -0400

    finished

--- Lists/list1/list1.py
+++ Lists/list1/list1.py
@@ -7,59 +7,64 @@ def create_list_from_tuple(t):
     """
     This function takes a tuple of elements and returns a list containing those elements of the tuple.
     """
-    pass  # implement me
+    return list(t)  # implement me
     
 def drop_last(lst):
     """
     This function takes a list and returns a list with the last item removed.
     """
-    pass  # implement me
+    return lst[:-1]  # implement me
 
 
 def drop_first_two(lst):
     """
     This function takes a list and returns a list with the first two items removed.
     """
-    pass  # implement me
+    return lst[2:]  # implement me
 
 def drop_mangle(lst):
     """
     This function takes a list and returns a list with the first two items AND last item removed.
     """
-    pass  # implement me
+    return lst[2:-1]  # implement me
 
 def add_item_front(lst, a):
     """
     This function takes a list and an item,
     returning the list with the item prepended to the list
     """
-    pass  # implement me
+    lst.insert(0, a)
+    return lst  # implement me
 
 def add_item_end(lst, a):
     """
     This function takes a list and an item,
     returning the list with the item appended to the list
     """
-    pass  # implement me
+    lst.append(a)
+    return lst  # implement me
 
 def add_list_to_list(lsta, lstb):
     """
     This function takes two lists and appends one to the other,
     returning a list
     """
-    pass  # implement me
+    return lsta + lstb  # implement me
 
 def list_and_list_to_tuple(lsta, lstb):
     """
     This function takes two lists and returns a tuple containing the two lists
     """
-    pass # implement me
+    return (lsta, lstb) # implement me
 
 def list_and_list_to_list(lsta, lstb):
     """
     This function takes two lists and returns a list containing the two lists
     """
-    pass # implement me
+    new_list = []
+    new_list.append(lsta)
+    new_list.append(lstb)
+    return new_list # implement me
 
 ##
 ##
@@ -69,39 +74,42 @@ def list_from_range(n):
     """
     This function returns list with 0..n as integers in a list
     """
-    pass # implement me
+    return list(range(0, n)) # implement me
 
 def list_from_range2(n, m):
     """
     This function returns list with n..m (without m) as integers in a list
     """
-    pass # implement me
+    return list(range(n, m)) # implement me
 
 def list_from_range3(n, m):
     """
     This function returns list with n..m (including m(!)) as integers in a list
     """
-    pass # implement me
+    return list(range(n, m+1)) # implement me
 
 def list_from_range4(n, m):
     """
     This function returns list with n..m (WITHOUT n and including m) as integers in a list
     """
-    pass # implement me
+    return list(range(n + 1, m + 1)) # implement me
 
 def list_from_range_by(n, step):
     """
     This function returns list with 0..n as integers by step in a list
     (read the test)
     """
-    pass # implement me
+
+    new_list = list(range(0, n, step))
+    return new_list
+    # implement me
 
 def rev_list(lst):
     """
     This function returns list which is a reverse of the argument list
     (read the test)
     """
-    pass # implement me
+    return lst[::-1]
   
 def concat_list_indexwise(lst1, lst2):
     """
@@ -110,36 +118,43 @@ def concat_list_indexwise(lst1, lst2):
     then the 1st index item, and so on till the last element. 
     Any leftover items will get added at the end of the new list.
     """
-    parseString # implement me
+    # result = []
+    # for i in range(0, min(len(lst1), len(lst2))):  #for loop method
+    #     result.append(lst1[i] + lst2[i])
+
+    result = [lst1[i] + lst2[i  ] for i in range(min(len(lst1), len(lst2)))] ## list comprehension
+    return result
 
 def square_each_item(lst):
     """
     This function returns list which each item in argument list has been squared
     (read the test)
     """
-    pass # implement me
+    return [i ** 2 for i in lst]
 
 def remove_empty_strs(lst):
      """
      Remove empty strings from the list of strings
      """
-     pass
+     return [i for i in lst if i != ""]
 
 
 def remove_item_from(lst, aaa):
     """
     Remove all occurrences of a specific item from a list.
     """
-    pass
+    result = [i for i in lst if i != aaa]
+    return result
 
 def leave_item_in(lst, aaa):
     """
     Leave all occurrences of a specific item in a list.
     """
-    pass
+    result = [i for i in lst if i == aaa]
+    return result
 
 def length_of(lst):
     """
     return the length of the list
     """
-    pass
+    return len(lst)
