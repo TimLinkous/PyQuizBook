e6b155997fc90413155df0100d5ab73973e32e2f
Tim L <timl@Zipcoders-MacBook-Pro.local>
2024-07-24 16:58:50 -0400

    dictionaries

--- Dicts/dict1/dict1.py
+++ Dicts/dict1/dict1.py
@@ -2,50 +2,52 @@ def create_dict_from_lists(keys, values):
     """
     This function returns a dict made from two lists.
     """
-    pass  # implement me
+    return dict(zip(keys, values))  # implement me
 
 def merge_two_dicts(d1, d2):
     """
     Merge two Python dictionaries into one
     """
-    pass  # implement me
+    return d1 | d2  # implement me
 
 def init_dict_with_values(lst, d1):
     """
     Create a dict with default values for each key listed.
 
     """
-    #
-    pass  # implement me
+    new_dict = dict.fromkeys(lst, d1)  # implement me
+    return new_dict
 
 def extract_keys_to_dict(datadict, keylist):
     """
     Create a dictionary by extracting the keylist from a given dictionary
     """
     #
-    pass  # implement me
+    return {key: datadict[key] for key in keylist}  # implement me
 
 def delete_keys_from_dict(datadict, keylist):
     """
     Delete a list of keys from a dictionary
     """
-    pass
+    for key in keylist:
+        datadict.pop(key, None)
+    return datadict
 
 def check_dict_for_key(datadict, key):
     """
     Check if a value exists in a dictionary
     (NO FOR loops!)
     """
-    pass
+    return key in datadict.values()
 
 def get_key_of_min_value(ddd):
-    """
+    """ 
     Get the key of the minimum value from a dictionary
     """
-    pass
+    return min(ddd, key = ddd.get)
 
 def get_key_of_max_value(ddd):
     """
     Get the key of the maximum value from a dictionary
     """
-    pass
+    return max(ddd, key = ddd.get)
